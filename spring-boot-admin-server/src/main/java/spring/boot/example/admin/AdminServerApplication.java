09e1f99fa6653aa141c5730957d5b4e8bba9966a
zhangshixi <shixizhang@foxmail.com>
2017-09-10 15:30:02 +0800

    spring-boot-admin-server initial commit

---
+++ spring-boot-admin-server/src/main/java/spring/boot/example/admin/AdminServerApplication.java
@@ -0,0 +1,20 @@
+package spring.boot.example.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created 2016-11-04 17:32:43
+ *
+ * @author Michael.Zhang
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
